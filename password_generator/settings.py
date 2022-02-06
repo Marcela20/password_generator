644cd131df4d62ad8d2af7dc32300066b0888363
Marcela20 <marcela.chrustowska98@gmail.com>
2022-02-06 20:16:23 +0100

    deleted files

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xpjuk#cna85h$f-ii=n9kzzxl9y%dyidbj^#57wpc9wnd^+h1='
+SECRET_KEY = 'django-insecure-xpjuk#cna85h$f-ii=n9kssxl9y%dyidbj^#57wpc9wnd^+h1='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
